26a492ab331f266b0165213b3612badd3cf8c3c1
kasawa1234 <sjz13718081358@gmail.com>
2024-05-09 10:55:35 +0800

    fix test scripts

--- test_scripts/test_conv.py
+++ test_scripts/test_conv.py
@@ -7,7 +7,7 @@ import time
 
 
 device = "cuda:0"
-iteration = 100
+iteration = 10
 time_python = 0
 time_conv = 0
 time_conv_parallel = 0
@@ -20,7 +20,7 @@ for i in range(iteration):
     #     np.random.randint(1, 513),
     #     np.random.randint(1, 513)
     # ]
-    shape_conv = [256, 8, 128, 128]
+    shape_conv = [256, 8, 32, 300]
     a = torch.randn(shape_conv, device=device)
     gamma = torch.ones(shape_conv[1], device=device)
     beta = torch.zeros(shape_conv[1], device=device)
@@ -42,9 +42,11 @@ for i in range(iteration):
     d = cppcuda_bn.bn_forward_conv_sram(a, gamma, beta)
     end_cppcuda_sram = time.time()
 
-    time_python += end_time_python - start_time_python
-    time_conv = end_cppcuda - start_cppcuda
-    time_conv_parallel = end_cppcuda_parallel - start_cppcuda_parallel
-    time_conv_sram += end_cppcuda_sram - start_cppcuda_sram
+    if i != 0:
+        time_python += end_time_python - start_time_python
+        time_conv += end_cppcuda - start_cppcuda
+        time_conv_parallel += end_cppcuda_parallel - start_cppcuda_parallel
+        time_conv_sram += end_cppcuda_sram - start_cppcuda_sram
+    # print(end_time_python - start_time_python, end_cppcuda - start_cppcuda, end_cppcuda_parallel - start_cppcuda_parallel, end_cppcuda_sram - start_cppcuda_sram)
 
 print(time_python, time_conv, time_conv_parallel, time_conv_sram)
